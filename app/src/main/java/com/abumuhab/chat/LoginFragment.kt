45faaef87343a78b4eced79116d304d1b17d4ef2
Abdulmalik <abumuhab98@gmail.com>
2021-09-06 20:00:55 +0100

    first commit

---
+++ app/src/main/java/com/abumuhab/chat/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.abumuhab.chat
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.abumuhab.chat.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,R.layout.fragment_login, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
+
+        return  binding.root
+    }
+}

e57d907359b7e5fd762af2b59226ff7049d51957
Abdulmalik <abumuhab98@gmail.com>
2021-09-06 22:56:53 +0100

    login screen

--- app/src/main/java/com/abumuhab/chat/SignupFragment.kt
+++ app/src/main/java/com/abumuhab/chat/SignupFragment.kt
@@ -2,7 +2,6 @@ package com.abumuhab.chat
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +10,10 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import com.abumuhab.chat.databinding.FragmentLoginBinding
+import com.abumuhab.chat.databinding.FragmentSignupBinding
 
-class LoginFragment : Fragment() {
+
+class SignupFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +23,16 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        val binding: FragmentSignupBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_signup, container, false)
         (activity as AppCompatActivity).supportActionBar?.hide()
 
-
-        binding.loginContainer.setOnClickListener {
+        binding.signupContainer.setOnClickListener {
             val imm = requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken,0)
         }
 
-        binding.signupText.setOnClickListener {
-            Log.i("HHH",",,,,,,,,,,,,,,,,,,,,,")
-            it.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+        binding.loginText.setOnClickListener {
+            it.findNavController().navigate(SignupFragmentDirections.actionSignupFragmentToLoginFragment())
         }
 
         return  binding.root

4aec1a3525f4038a75c0efb0f9f030469914509f
Abdulmalik <abumuhab98@gmail.com>
2021-09-07 09:33:05 +0100

    oneTapAuth implementation

--- app/src/main/java/com/abumuhab/chat/LoginFragment.kt
+++ app/src/main/java/com/abumuhab/chat/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.abumuhab.chat
 
 import android.app.Activity
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,8 +13,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.abumuhab.chat.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 
 class LoginFragment : Fragment() {
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var signUpRequest: BeginSignInRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +44,66 @@ class LoginFragment : Fragment() {
             it.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
         }
 
+        oneTapClient = Identity.getSignInClient((activity as AppCompatActivity))
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true).build()
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(true)
+                    .setServerClientId("447045430784-kll64g5872glhok59lb6h1c5a9okdoin.apps.googleusercontent.com")
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId("447045430784-kll64g5872glhok59lb6h1c5a9okdoin.apps.googleusercontent.com")
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build()
+
+        binding.loginWithGoogle.setOnClickListener{
+            oneTapAuth()
+        }
+
         return  binding.root
+    }
+
+    //TODO: refractor
+    private fun oneTapAuth() {
+        oneTapClient.beginSignIn(signInRequest).addOnSuccessListener {
+            try {
+                Log.i("CLASS", "here")
+                startIntentSenderForResult(it.pendingIntent.intentSender, 1, null, 0, 0, 0, null)
+            } catch (e: IntentSender.SendIntentException) {
+                Log.i("CLASS", "Could not start oneTap UI")
+            }
+        }.addOnFailureListener{e1->
+            Log.i("CLASS", e1.localizedMessage)
+            oneTapClient.beginSignIn(signUpRequest)
+                .addOnSuccessListener{
+                    try {
+                        startIntentSenderForResult(
+                            it.pendingIntent.intentSender, 2,
+                            null, 0, 0, 0,null)
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e("CLASS", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener{ e2->
+                    // No Google Accounts found. Just continue presenting the signed-out UI.
+                    Log.d("Class", e2.localizedMessage)
+                }
+
+        }
     }
 }
--- app/src/main/java/com/abumuhab/chat/SignupFragment.kt
+++ app/src/main/java/com/abumuhab/chat/SignupFragment.kt
@@ -1,7 +1,9 @@
 package com.abumuhab.chat
 
 import android.app.Activity
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +13,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.abumuhab.chat.databinding.FragmentSignupBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 
 
 class SignupFragment : Fragment() {
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var signUpRequest: BeginSignInRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +31,84 @@ class SignupFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentSignupBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_signup, container, false)
+        val binding: FragmentSignupBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
         (activity as AppCompatActivity).supportActionBar?.hide()
 
         binding.signupContainer.setOnClickListener {
-            val imm = requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(it.windowToken,0)
+            val imm =
+                requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
 
         binding.loginText.setOnClickListener {
-            it.findNavController().navigate(SignupFragmentDirections.actionSignupFragmentToLoginFragment())
+            it.findNavController()
+                .navigate(SignupFragmentDirections.actionSignupFragmentToLoginFragment())
         }
 
-        return  binding.root
+        binding.signupWithGoogle.setOnClickListener {
+            oneTapAuth()
+        }
+
+        oneTapClient = Identity.getSignInClient((activity as AppCompatActivity))
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true).build()
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(true)
+                    .setServerClientId("447045430784-kll64g5872glhok59lb6h1c5a9okdoin.apps.googleusercontent.com")
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId("447045430784-kll64g5872glhok59lb6h1c5a9okdoin.apps.googleusercontent.com")
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build()
+
+
+        return binding.root
     }
+
+    //TODO: refractor
+    private fun oneTapAuth() {
+        oneTapClient.beginSignIn(signInRequest).addOnSuccessListener {
+            try {
+                Log.i("CLASS", "here")
+                startIntentSenderForResult(it.pendingIntent.intentSender, 1, null, 0, 0, 0, null)
+            } catch (e: IntentSender.SendIntentException) {
+                Log.i("CLASS", "Could not start oneTap UI")
+            }
+        }.addOnFailureListener{e1->
+            Log.i("CLASS", e1.localizedMessage)
+            oneTapClient.beginSignIn(signUpRequest)
+                .addOnSuccessListener{
+                    try {
+                        startIntentSenderForResult(
+                            it.pendingIntent.intentSender, 2,
+                            null, 0, 0, 0,null)
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e("CLASS", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener{ e2->
+                    // No Google Accounts found. Just continue presenting the signed-out UI.
+                    Log.d("Class", e2.localizedMessage)
+                }
+
+        }
+    }
+
+
 }

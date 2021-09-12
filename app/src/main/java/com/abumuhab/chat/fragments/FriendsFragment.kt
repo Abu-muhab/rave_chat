132ad303c080143c55f879c2c52bcb460f43a5d6
Abdulmalik <abumuhab98@gmail.com>
2021-09-12 10:40:52 +0100

    close frineds list init

--- app/src/main/java/com/abumuhab/chat/fragments/FriendsFragment.kt
+++ app/src/main/java/com/abumuhab/chat/fragments/FriendsFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.abumuhab.chat.R
+import com.abumuhab.chat.databinding.AvatarBinding
 import com.abumuhab.chat.databinding.FragmentFriendsBinding
 
 class FriendsFragment : Fragment() {
@@ -18,6 +19,25 @@ class FriendsFragment : Fragment() {
         val binding: FragmentFriendsBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_friends, container, false)
         (activity as AppCompatActivity).supportActionBar?.hide()
+
+        val avatars = arrayOf(
+            R.drawable.avatar_1,
+            R.drawable.avatar_16,
+            R.drawable.avatar_22,
+            R.drawable.avatar_31,
+            R.drawable.avatar_33,
+            R.drawable.avatar_1,
+            R.drawable.avatar_4,
+            R.drawable.avatar_5,
+            R.drawable.avatar_6,
+        )
+
+        avatars.forEach {
+            val avatarBinding = AvatarBinding.inflate(inflater, binding.friendsPreviewLayout, false)
+            avatarBinding.resourceId = it
+            binding.lifecycleOwner = this
+            binding.friendsPreviewLayout.addView(avatarBinding.root)
+        }
         return binding.root
     }
 }
